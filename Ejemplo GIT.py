01363174d1c2b50dc4180c4cddc3c0975fe4a338
ericktorresm92 <147800758+ericktorresm92@users.noreply.github.com>
2024-12-17 20:52:01 -0800

    Se añada la variable d
    La variable d sirve para el cálculo del total final. Es una variable que no puede olvidarse dentro del proceso.

--- Ejemplo GIT.py
+++ Ejemplo GIT.py
@@ -1,6 +1,10 @@
-# Prueba Python, primera prueba, revisión
+# Prueba Python
+# Se añadió la variable "d"
 
 a = 10
 b = 30
-c = 30 * 10
-print('Resultado=', 300)
+c = 30 * 10 * 5
+print('Resultado=', c)
+
+d = c * 100
+print ('Resulatdo Total=', d)
