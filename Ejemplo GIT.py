e82be811bdde16d7f741516413697d051adecaff
ericktorresm92 <147800758+ericktorresm92@users.noreply.github.com>
2024-12-18 14:07:03 -0800

    Actualizando cambios

--- Ejemplo GIT.py
+++ Ejemplo GIT.py
@@ -6,9 +6,16 @@ b = 30
 c = 30 * 10 * 5
 print('Resultado=', c)
 
+<<<<<<< HEAD
+## Cambiando el factor 102 según requerimiento del negocio
+## Cambiando el factor 110 según Junta de Control 
+
+d = c * 110
+=======
 ## Cambiando el factor 102 según requerimiento del negocio
 ## Cambiando el factor 110 según Junta Control
 ## Cambio a factor 200 por Directorio
 
 d = c * 200
+>>>>>>> 37522a8d9facedfa5c71fac108fe510358bef85e
 print ('Resulatdo Total=', d)
