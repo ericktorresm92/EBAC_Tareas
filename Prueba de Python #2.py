b60ca4f1de85da8d47578d2f6a7d985c6ea7538a
ericktorresm92 <147800758+ericktorresm92@users.noreply.github.com>
2024-12-20 15:24:40 -0800

    y de nuevo

--- Prueba de Python #2.py
+++ Prueba de Python #2.py
@@ -1,6 +1,6 @@
 # Prueba de Python #2
 # se añadio la variable "c"
-# comentario para visual code, modificando de nuevo
+# comentario para visual code, modificando de nuevo, y de nuevo
 a = 10
 b = 30
 c = 30 * 10 * 5
